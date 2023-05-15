9025494185f665d135ab6f606e18010b800b207b
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-05-15 20:09:51 -0300

    path aliases and vitest globals

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
 import { AnswersRespoitory } from '../repositories/answers-repository'
 import { Answer } from '../entities/answer'
